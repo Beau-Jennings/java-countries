bd98358339139cc7c9ccdd770ec3d31ac9a4e847
Beau Jennings <redalthazar@gmail.com>
2021-04-08 15:47:17 -0500

    built Countries Model and created CountriesController

---
+++ src/test/java/com/lambda/school/countries/CountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda.school.countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
